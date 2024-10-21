e2c083ec45bcf71df3bffc4129e5f6e5af4bc265
AlexStry <150042828+AlexStry@users.noreply.github.com>
2024-10-20 23:00:29 -0600

    Update SistemaNegocios.cpp

--- SistemaNegocios.cpp
+++ SistemaNegocios.cpp
@@ -1,5 +1,7 @@
 #include "SistemaNegocios.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 void SistemaNegocios::agregarEmpleado(int id, string nombre, int antiguedad, int salario){
     Empleado nuevoEmpleado(id, nombre, antiguedad, salario);
@@ -24,4 +26,24 @@ void SistemaNegocios::mostrarEmpleados() const{
     for(const auto& empleado : empleados){
         std::cout<<"ID: "<<empleado.getId()<<", Nombre: "<<empleado.getNombre()<<", Antiguedad: "<<empleado.getAntiguedad()<<", Salario: "<<empleado.getSalario()<<std::endl;
     }
+}
+
+void SistemaNegocios::cargarEmpleadosDesdeArchivo(const std::string& nombreArchivo) {
+    std::ifstream archivo(nombreArchivo);
+    if (!archivo.is_open()) {
+        std::cerr << "No se pudo abrir el archivo " << nombreArchivo << std::endl;
+        return;
+    }
+    std::string linea;
+    while (std::getline(archivo, linea)) {
+        std::istringstream stream(linea);
+        int id, antiguedad, salario;
+        std::string nombre;
+        if (stream >> id >> nombre >> antiguedad >> salario) {
+            agregarEmpleado(id, nombre, antiguedad, salario);
+        } else {
+            std::cerr << "Error al leer la línea: " << linea << std::endl;
+        }
+    }
+    archivo.close();
 }
