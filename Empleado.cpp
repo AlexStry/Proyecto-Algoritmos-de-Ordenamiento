83e2dd7f74d958f0f950039bd28c13f861e7137a
AlexStry <150042828+AlexStry@users.noreply.github.com>
2024-11-25 08:23:27 -0600

    Update Empleado.cpp

--- Empleado.cpp
+++ Empleado.cpp
@@ -1,35 +1,21 @@
 #include "Empleado.h"
-#include <iostream>
-using namespace std;
 
-Empleado::Empleado(): salario(0), antiguedad(0) {}
+Empleado::Empleado(int id, const std::string& nombre, int antiguedad, int salario)
+    : id(id), nombre(nombre), antiguedad(antiguedad), salario(salario){}
 
-Empleado::Empleado(int id_, string nombre_, int antiguedad_, int salario_) : id(id_), nombre(nombre_), antiguedad(antiguedad_), salario(salario_) {}
-
-int Empleado::getId() const {
+int Empleado::getId() const{
     return id;
 }
 
-string Empleado::getNombre() const{
+std::string Empleado::getNombre() const{
     return nombre;
 }
+
 
 int Empleado::getAntiguedad() const {
     return antiguedad;
 }
 
-int Empleado::getSalario() const {
+int Empleado::getSalario() const{
     return salario;
-}
-
-void Empleado::setNombre(string nombre_){
-    nombre = nombre_;
-}
-
-void Empleado::setAntiguedad(int antiguedad_){
-    antiguedad = antiguedad_;
-}
-
-void Empleado::setSalario(int salario_){
-    salario = salario_;
 }
