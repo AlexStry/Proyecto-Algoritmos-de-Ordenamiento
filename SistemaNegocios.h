f252b37ac52652fe6a255b80a332c886b0da87a9
AlexStry <150042828+AlexStry@users.noreply.github.com>
2024-11-25 08:23:41 -0600

    Update SistemaNegocios.h

--- SistemaNegocios.h
+++ SistemaNegocios.h
@@ -1,20 +1,22 @@
 #ifndef SISTEMANEGOCIOS_H
 #define SISTEMANEGOCIOS_H
 #include <list>
-#include <vector>
-#include "Empleado.h"
 #include <string>
+#include "Empleado.h"
 
-class SistemaNegocios{
-    public:
-    void agregarEmpleado(int, string, int, int);
+class SistemaNegocios {
+public:
+    void agregarEmpleado(int id, const std::string& nombre, int antiguedad, int salario);
     void ordenarAntiguedad();
     void mostrarEmpleados() const;
     void cargarEmpleadosDesdeArchivo(const std::string& nombreArchivo);
+    void guardarEmpleadosEnArchivo(const std::string& nombreArchivo) const;
+    void buscarPorId(int id) const;
+    void buscarPorRangoSalario(int min, int max) const;
+    void generarEstadisticas() const;
 
-    private:
+private:
     std::list<Empleado> empleados;
 };
-
 
 #endif
